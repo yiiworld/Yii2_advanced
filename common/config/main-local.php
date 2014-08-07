fb781b41c727631cbadcebd52d6e4abf634f746c
BigKuCha <nevermore1989@vip.qq.com>
2014-08-08 07:31:45 +0800

    config files

---
+++ common/config/main-local.php
@@ -0,0 +1,25 @@
+<?php
+$db = require(__DIR__ . '/db.php');
+return [
+    'components' => [
+        'db' => $db['db'],
+        'mail' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => true,
+        ],
+        'urlManager'=>[
+            'class'=>  yii\web\UrlManager::className(),
+            'enablePrettyUrl'=>true,
+            'showScriptName'=>false,
+            'rules'=>[
+                '<controller:\w+>/<id:\d+>'=>'<controller>/view',
+                '<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
+                '<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
+            ],
+        ],
+    ],
+];
