2fedd20dfe9ac53113ae766a4f064a82187acf5d
BigKuCha <nevermore1989@vip.qq.com>
2014-08-13 20:38:51 +0800

    cookie

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -1,5 +1,6 @@
 <?php
 use yii\helpers\Html;
+
 \backend\assets\AppAsset::register($this);
 ?>
 <?php $this->beginPage() ?>
@@ -173,7 +174,6 @@ use yii\helpers\Html;
                                 设置
                             </a>
                         </li>
-
                         <li>
                             <a href="#">
                                 <i class="icon-user"></i>
@@ -200,169 +200,170 @@ use yii\helpers\Html;
 </div>
 
 <div class="main-container" id="main-container">
-<script type="text/javascript">
-    try {
-        ace.settings.check('main-container', 'fixed')
-    } catch (e) {
-    }
-</script>
-
-<div class="main-container-inner">
-<a class="menu-toggler" id="menu-toggler" href="#">
-    <span class="menu-text"></span>
-</a>
-
-<div class="sidebar" id="sidebar">
     <script type="text/javascript">
         try {
-            ace.settings.check('sidebar', 'fixed')
+            ace.settings.check('main-container', 'fixed')
         } catch (e) {
         }
     </script>
 
-    <div class="sidebar-shortcuts" id="sidebar-shortcuts">
-        <div class="sidebar-shortcuts-large" id="sidebar-shortcuts-large">
-            <button class="btn btn-success">
-                <i class="icon-signal"></i>
-            </button>
+    <div class="main-container-inner">
+        <a class="menu-toggler" id="menu-toggler" href="#">
+            <span class="menu-text"></span>
+        </a>
 
-            <button class="btn btn-info">
-                <i class="icon-pencil"></i>
-            </button>
+        <div class="sidebar" id="sidebar">
+            <script type="text/javascript">
+                try {
+                    ace.settings.check('sidebar', 'fixed')
+                } catch (e) {
+                }
+            </script>
 
-            <button class="btn btn-warning">
-                <i class="icon-group"></i>
-            </button>
+            <div class="sidebar-shortcuts" id="sidebar-shortcuts">
+                <div class="sidebar-shortcuts-large" id="sidebar-shortcuts-large">
+                    <button class="btn btn-success">
+                        <i class="icon-signal"></i>
+                    </button>
 
-            <button class="btn btn-danger">
-                <i class="icon-cogs"></i>
-            </button>
+                    <button class="btn btn-info">
+                        <i class="icon-pencil"></i>
+                    </button>
+
+                    <button class="btn btn-warning">
+                        <i class="icon-group"></i>
+                    </button>
+
+                    <button class="btn btn-danger">
+                        <i class="icon-cogs"></i>
+                    </button>
+                </div>
+
+                <div class="sidebar-shortcuts-mini" id="sidebar-shortcuts-mini">
+                    <span class="btn btn-success"></span>
+
+                    <span class="btn btn-info"></span>
+
+                    <span class="btn btn-warning"></span>
+
+                    <span class="btn btn-danger"></span>
+                </div>
+            </div>
+            <!-- #sidebar-shortcuts -->
+            <?= $this->render('/_menucache/' . Yii::$app->user->identity->role, ['column' => Yii::$app->controller->id . '/' . Yii::$app->controller->action->id]) ?>
+            <!-- /.nav-list -->
+
+            <div class="sidebar-collapse" id="sidebar-collapse">
+                <i class="icon-double-angle-left" data-icon1="icon-double-angle-left"
+                   data-icon2="icon-double-angle-right"></i>
+            </div>
+
+            <script type="text/javascript">
+                try {
+                    ace.settings.check('sidebar', 'collapsed')
+                } catch (e) {
+                }
+            </script>
         </div>
 
-        <div class="sidebar-shortcuts-mini" id="sidebar-shortcuts-mini">
-            <span class="btn btn-success"></span>
+        <div class="main-content">
+            <div class="breadcrumbs" id="breadcrumbs">
+                <script type="text/javascript">
+                    try {
+                        ace.settings.check('breadcrumbs', 'fixed')
+                    } catch (e) {
+                    }
+                </script>
 
-            <span class="btn btn-info"></span>
+                <ul class="breadcrumb">
+                    <li>
+                        <i class="icon-home home-icon"></i>
+                        <a href="/">首页</a>
+                    </li>
+                    <li class="active">控制台</li>
+                </ul>
+                <!-- .breadcrumb -->
 
-            <span class="btn btn-warning"></span>
-
-            <span class="btn btn-danger"></span>
-        </div>
-    </div>
-    <!-- #sidebar-shortcuts -->
-    <?= $this->render('/_menucache/'.Yii::$app->user->identity->role,['column'=>Yii::$app->controller->id.'/'.Yii::$app->controller->action->id]) ?>
-    <!-- /.nav-list -->
-
-    <div class="sidebar-collapse" id="sidebar-collapse">
-        <i class="icon-double-angle-left" data-icon1="icon-double-angle-left" data-icon2="icon-double-angle-right"></i>
-    </div>
-
-    <script type="text/javascript">
-        try {
-            ace.settings.check('sidebar', 'collapsed')
-        } catch (e) {
-        }
-    </script>
-</div>
-
-<div class="main-content">
-    <div class="breadcrumbs" id="breadcrumbs">
-        <script type="text/javascript">
-            try {
-                ace.settings.check('breadcrumbs', 'fixed')
-            } catch (e) {
-            }
-        </script>
-
-        <ul class="breadcrumb">
-            <li>
-                <i class="icon-home home-icon"></i>
-                <a href="/">首页</a>
-            </li>
-            <li class="active">控制台</li>
-        </ul>
-        <!-- .breadcrumb -->
-
-        <div class="nav-search" id="nav-search">
-            <form class="form-search">
+                <div class="nav-search" id="nav-search">
+                    <form class="form-search">
                                 <span class="input-icon">
                                     <input type="text" placeholder="Search ..." class="nav-search-input"
                                            id="nav-search-input" autocomplete="off"/>
                                     <i class="icon-search nav-search-icon"></i>
                                 </span>
-            </form>
-        </div>
-        <!-- #nav-search -->
-    </div>
-
-    <div class="page-content">
-        <div class="row">
-            <div class="col-xs-12">
-                <!-- PAGE CONTENT BEGINS -->
-                <?= $content ?>
+                    </form>
+                </div>
+                <!-- #nav-search -->
             </div>
-            <!-- /.col -->
-        </div>
-        <!-- /.row -->
-    </div>
-    <!-- /.page-content -->
-</div>
-<!-- /.main-content -->
 
-<div class="ace-settings-container" id="ace-settings-container">
-    <div class="btn btn-app btn-xs btn-warning ace-settings-btn" id="ace-settings-btn">
-        <i class="icon-cog bigger-150"></i>
-    </div>
-
-    <div class="ace-settings-box" id="ace-settings-box">
-        <div>
-            <div class="pull-left">
-                <select id="skin-colorpicker" class="hide">
-                    <option data-skin="default" value="#438EB9">#438EB9</option>
-                    <option data-skin="skin-1 " value="#222A2D">#222A2D</option>
-                    <option data-skin="skin-2" value="#C6487E">#C6487E</option>
-                    <option data-skin="skin-3" value="#D0D0D0">#D0D0D0</option>
-                </select>
+            <div class="page-content">
+                <div class="row">
+                    <div class="col-xs-12">
+                        <!-- PAGE CONTENT BEGINS -->
+                        <?= $content ?>
+                    </div>
+                    <!-- /.col -->
+                </div>
+                <!-- /.row -->
             </div>
-            <span>&nbsp; 选择皮肤</span>
+            <!-- /.page-content -->
         </div>
+        <!-- /.main-content -->
 
-        <div>
-            <input type="checkbox" class="ace ace-checkbox-2" id="ace-settings-navbar"/>
-            <label class="lbl" for="ace-settings-navbar"> 固定导航条</label>
-        </div>
+        <div class="ace-settings-container" id="ace-settings-container">
+            <div class="btn btn-app btn-xs btn-warning ace-settings-btn" id="ace-settings-btn">
+                <i class="icon-cog bigger-150"></i>
+            </div>
 
-        <div>
-            <input type="checkbox" class="ace ace-checkbox-2" id="ace-settings-sidebar"/>
-            <label class="lbl" for="ace-settings-sidebar"> 固定滑动条</label>
-        </div>
+            <div class="ace-settings-box" id="ace-settings-box">
+                <div>
+                    <div class="pull-left">
+                        <select id="skin-colorpicker" class="hide">
+                            <option data-skin="default" value="#438EB9">#438EB9</option>
+                            <option data-skin="skin-1 " value="#222A2D">#222A2D</option>
+                            <option data-skin="skin-2" value="#C6487E">#C6487E</option>
+                            <option data-skin="skin-3" value="#D0D0D0">#D0D0D0</option>
+                        </select>
+                    </div>
+                    <span>&nbsp; 选择皮肤</span>
+                </div>
 
-        <div>
-            <input type="checkbox" class="ace ace-checkbox-2" id="ace-settings-breadcrumbs"/>
-            <label class="lbl" for="ace-settings-breadcrumbs">固定面包屑</label>
-        </div>
+                <div>
+                    <input type="checkbox" class="ace ace-checkbox-2" id="ace-settings-navbar"/>
+                    <label class="lbl" for="ace-settings-navbar"> 固定导航条</label>
+                </div>
 
-        <div>
-            <input type="checkbox" class="ace ace-checkbox-2" id="ace-settings-rtl"/>
-            <label class="lbl" for="ace-settings-rtl">切换到左边</label>
-        </div>
+                <div>
+                    <input type="checkbox" class="ace ace-checkbox-2" id="ace-settings-sidebar"/>
+                    <label class="lbl" for="ace-settings-sidebar"> 固定滑动条</label>
+                </div>
 
-        <div>
-            <input type="checkbox" class="ace ace-checkbox-2" id="ace-settings-add-container"/>
-            <label class="lbl" for="ace-settings-add-container">
-                切换窄屏
-                <b></b>
-            </label>
+                <div>
+                    <input type="checkbox" class="ace ace-checkbox-2" id="ace-settings-breadcrumbs"/>
+                    <label class="lbl" for="ace-settings-breadcrumbs">固定面包屑</label>
+                </div>
+
+                <div>
+                    <input type="checkbox" class="ace ace-checkbox-2" id="ace-settings-rtl"/>
+                    <label class="lbl" for="ace-settings-rtl">切换到左边</label>
+                </div>
+
+                <div>
+                    <input type="checkbox" class="ace ace-checkbox-2" id="ace-settings-add-container"/>
+                    <label class="lbl" for="ace-settings-add-container">
+                        切换窄屏
+                        <b></b>
+                    </label>
+                </div>
+            </div>
         </div>
+        <!-- /#ace-settings-container -->
     </div>
-</div>
-<!-- /#ace-settings-container -->
-</div>
-<!-- /.main-container-inner -->
+    <!-- /.main-container-inner -->
 
-<a href="#" id="btn-scroll-up" class="btn-scroll-up btn btn-sm btn-inverse">
-    <i class="icon-double-angle-up icon-only bigger-110"></i>
-</a>
+    <a href="#" id="btn-scroll-up" class="btn-scroll-up btn btn-sm btn-inverse">
+        <i class="icon-double-angle-up icon-only bigger-110"></i>
+    </a>
 </div>
 <!-- /.main-container -->
 
@@ -372,13 +373,13 @@ use yii\helpers\Html;
 </script>
 
 <script type="text/javascript">
-    $(document).ready(function(){
+    $(document).ready(function () {
         //自动打开菜单
         var active = $('#active');
-        active.attr('class','active');
+        active.attr('class', 'active');
         var parentli = active.parent().parent();
-        parentli.attr('class','active open');
-        parentli.parent().parent().attr('class','active open');
+        parentli.attr('class', 'active open');
+        parentli.parent().parent().attr('class', 'active open');
     })
 </script>
 
